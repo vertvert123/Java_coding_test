8df0c30be170e11e77885549211cdaeb51a6244e
vertvert123 <vertvert123@naver.com>
2022-01-03 21:21:54 +0900

    Clear!

--- 1단계/Number_6.java
+++ 1단계/Number_6.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Number_6 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(a - b);
+        scan.close();
+
+
+    }
 }
