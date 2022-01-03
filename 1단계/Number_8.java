023ba76f8c4579eb3e1bed51e27009d197cbbd33
vertvert123 <vertvert123@naver.com>
2022-01-03 21:28:24 +0900

    Clear!

---
+++ 1단계/Number_8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Number_8 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+
+        System.out.println(a/b);
+        scan.close();
+
+
+    }
+}
