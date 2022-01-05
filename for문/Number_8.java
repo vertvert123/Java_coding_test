394e3f82511718045dc172e954906e0241a8c640
vertvert123 <vertvert123@naver.com>
2022-01-05 22:39:16 +0900

    Clear!

---
+++ for문/Number_8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Number_8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+
+        for (int B = 0; B < A; B++) {
+            int C = scan.nextInt();
+            int D = scan.nextInt();
+            System.out.println("Case #"+(B+1)+": "+C+" + "+D+" = "+(C + D));
+        }
+        scan.close();
+
+
+    }
+}
