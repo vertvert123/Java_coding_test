f58eb01ebfd7c8225573cc13347ecf4a0738ee1b
vertvert123 <vertvert123@naver.com>
2022-01-05 21:57:04 +0900

    Clear!

---
+++ for문/Number_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Number_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+
+        for (int B = 0; B < A; B++) {
+            int C = scan.nextInt();
+            int D = scan.nextInt();
+            System.out.println(C + D);
+        }
+        scan.close();
+
+
+    }
+
+}
