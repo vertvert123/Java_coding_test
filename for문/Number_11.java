8529773edee93a1f89e028b6f406b8cc971babc4
vertvert123 <vertvert123@naver.com>
2022-01-07 22:24:06 +0900

    아직 못풀었음

---
+++ for문/Number_11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Number_11{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+        
+        
+
+    }
+}
