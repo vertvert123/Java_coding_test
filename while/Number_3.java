241ce18068ac3c072de0e84b9f105f1944bdf334
vertvert123 <vertvert123@naver.com>
2022-01-09 23:20:33 +0900

    해결못함

---
+++ while/Number_3.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Number_3 {
+
+    public static int chkcount(int A){
+        int Answer;
+        ArrayList<Integer> list_int = new ArrayList<Integer>();
+
+
+        if(A<10){
+            list_int.add(A*10);
+        }
+        else{
+            list_int.add(A);
+        }
+
+        while(true){
+            if (list_int.get(0)==list_int.get(list_int.size()-1)){
+                Answer =list_int.size();
+                break;
+            }
+            else if(list_int.get(list_int.size()-1)>=10){
+                list_int.add((A%10)*10+((A%10)+(A/10)));
+            }
+        }
+        return Answer;
+
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+
+        chkcount(A);
+        scan.close();
+        
+        
+    }
+    
+}
