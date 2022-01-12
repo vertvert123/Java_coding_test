8c2707fbe22ffbf5ea87163fa2ee5a635151ce90
vertvert123 <vertvert123@naver.com>
2022-01-13 02:13:27 +0900

    Clear!

--- for문/Number_11.java
+++ for문/Number_11.java
@@ -5,9 +5,17 @@ public class Number_11{
         Scanner scan = new Scanner(System.in);
         int A = scan.nextInt();
         int B = scan.nextInt();
-        int C = scan.nextInt();
-        
-        
 
+        int[] C =new int[A];
+
+        for(int i=0; i<A; i++){
+            int D = scan.nextInt();
+            C[i] = D;
+            if(C[i]<B){
+                System.out.println(C[i]);
+            }
+        }
+        
+        scan.close();
     }
 }
