d7597e7e2002f9ec0904999155f56c5dee970513
vertvert123 <vertvert123@naver.com>
2022-01-14 23:20:19 +0900

    배열생성

--- Array/Number_1.java
+++ Array/Number_1.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Number_1{
     public static void main(String[] args) {
-        int a[] = {};
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B[] = new int[A];
+
+        for(int C = 0; C<A; C++){
+            int D
+        }
     }
 }
