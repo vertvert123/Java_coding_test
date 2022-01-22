0013ac95e4395057e213f929c125aaba61e2458e
vertvert123 <vertvert123@naver.com>
2022-01-22 20:39:17 +0900

    Clear!

--- Array/Number_1.java
+++ Array/Number_1.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
 
-public class Number_1{
+public class Number_1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int A = scan.nextInt();
         int B[] = new int[A];
-        int MAX=B[0];
-        int MIN=B[0];
+        
 
-        for(int C = 0; C<A; C++){
+        for (int C = 0; C < A; C++) {
             int D = scan.nextInt();
             B[C] = D;
         }
+        int MAX = B[0];
+        int MIN = B[0];
 
-        for(int C = 0; C<A; C++){
-            if(MAX < B[C]){
+        for (int C = 0; C < A; C++) {
+            if (MAX < B[C]) {
                 MAX = B[C];
-            }
-        }
 
-        for(int C= 0; C<A; C++){
-            if(MIN > B[C]){
+
+            }
+            if (MIN > B[C]) {
                 MIN = B[C];
             }
+
+    
         }
 
-
-        System.out.println(MAX);
-        System.out.println(MIN);
+        System.out.println(MIN+ " "+MAX);
+        
         scan.close();
     }
 }
