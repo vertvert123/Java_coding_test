454d4caaae4c7ad1e401473661369222386ba9d6
vertvert123 <vertvert123@naver.com>
2022-01-23 23:40:21 +0900

    bufferread&write

--- for문/Number_4.java
+++ for문/Number_4.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class Number_4 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int B = Integer.parseInt(bf.readLine());
+        for(int i =0; i<B; i++){
+            st = new StringTokenizer(bf.readLine());
+            bw.write((Integer.parseInt(st.nextToken()))+Integer.parseInt(st.nextToken())+"\n");
+        }
+        bw.close();
     }
 }
