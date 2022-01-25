a9353c190e0b021958eba487ac6170100c3c0947
vertvert123 <vertvert123@naver.com>
2022-01-26 00:12:21 +0900

    Clear

---
+++ 사칙연산/Number_12.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Number_12 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        String  A = scan.nextLine();
+
+        System.out.println(A+"??!");
+
+        scan.close();
+    }
+}
---
+++ 사칙연산/Number_13.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Number_13 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        A = A-543;
+        System.out.println(A);
+        scan.close();
+    }   
+}
