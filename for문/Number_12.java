81632f22a7fcab445d818788ebf1f6c48f4c7ae8
vertvert123 <vertvert123@naver.com>
2022-02-03 22:18:07 +0900

    Clear!

---
+++ for문/Number_12.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Number_12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B[] = new int[A];
+        int result = 0;
+        String C = scan.next();
+        String D[] = C.split("");
+
+        for (int i = 0; i < D.length; i++) {
+            B[i] = Integer.parseInt(D[i]);
+        }
+
+        for (int i = 0; i < B.length; i++) {
+            result += B[i];
+        }
+        scan.close();
+        System.out.println(result);
+    }
+}
