4ec4a12baeeffaf2f50d944d356dff36b9703558
vertvert123 <vertvert123@naver.com>
2022-02-05 00:08:46 +0900

    Clear!

--- Array/Number_7.java
+++ Array/Number_7.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class Number_7 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A =  scan.nextInt();
+        int B[] = new int[A];
+        for(int i = 0; i<B.length; i++){
+            int E = 0;
+            Double F = 0.0;
+            Double G = 0.0;
+            int C = scan.nextInt();
+            int D [] = new int[C];
+
+            for(int j = 0; j<D.length; j++){
+                D[j] = scan.nextInt();
+                E += D[j];
+            }
+            F = (double) (E/D.length);
+
+            for(int k = 0; k<D.length; k++){
+                if(D[k]>F){
+                    G +=1;
+                }
+                else{
+                }
+            }
+            System.out.println(String.format("%.3f", G/D.length*100)+"%");// 소수점자리 출력
+        }
+        scan.close();
+
+        
+    }
 }
