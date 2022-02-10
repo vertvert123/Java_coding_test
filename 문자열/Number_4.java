1025ee65d47ea7c04a46a8347909cecf85add259
vertvert123 <vertvert123@naver.com>
2022-02-10 22:15:06 +0900

    잠시대기

---
+++ 문자열/Number_4.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Number_4{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int R = scan.nextInt();
+        String S = scan.next();
+        List<String> P = new ArrayList<>();
+        
+        
+    }
+}
