fb1ce47f1e4ce3973df35578a6799e74265611be
vertvert123 <vertvert123@naver.com>
2022-06-07 01:32:04 +0900

    Clear!

---
+++ 사칙연산/Num_25083.java
@@ -0,0 +1,5 @@
+public class Num_25083 {
+public static void main(String[] args) {
+  System.out.print(         "         ,r\'\"7\nr`-_   ,'  ,/\n \\. \". L_r\'\n   `~\\/\n      |\n      |");  
+}
+}
