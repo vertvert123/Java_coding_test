a864957b0d1514136b0b6fbe52d282535acafa4c
vertvert123 <vertvert123@naver.com>
2022-09-30 11:53:12 +0900

    ㅎㅅㅎ

---
+++ DFSBFS/test.java
@@ -0,0 +1,13 @@
+
+
+public class test {
+    public static void main(String[] args) {
+        boolean te[] = new boolean[10];
+
+        for(int i=0; i<te.length; i++){
+            System.out.println(te[i]);
+        }
+
+
+    }
+}
